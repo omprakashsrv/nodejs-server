6568c059b182464340414cae36694deb025c7023
OMPRAKASH <om@recibotech.com>
2021-07-18 19:23:14 +0530

    initial commit

--- digio/client.js
+++ digio/client.js
@@ -2,6 +2,9 @@ const baseUrl = "https://ext.digio.in:444/";
 let clientId = process.env.CLIENT_ID || 'AIZ67DUSNZ8TGWJV4DZ7DI3T5Z2LN2W2';
 let clientSecret = process.env.CLIENT_SECRET || 'ASN9AVKHU6HF41KTU71G3KNXLG1ET7BC';
 let authToken = Buffer.from(clientId + ":" + clientSecret).toString('base64')
+let headers = {
+    "Authorization": "Basic " + authToken
+};
 
 const got = require('got');
 
@@ -18,9 +21,7 @@ exports.uploadPdf = async function (file) {
             "file_name": "test.pdf",
             "file_data": file.buffer.toString('base64')
         },
-        headers: {
-            "Authorization": "Basic " + authToken
-        },
+        headers: headers,
         responseType: 'json'
     });
 }
@@ -28,8 +29,6 @@ exports.uploadPdf = async function (file) {
 exports.downloadPdf = function (documentId) {
     return got.stream(baseUrl + 'v2/client/document/download', {
         searchParams: {document_id: documentId},
-        headers: {
-            "Authorization": "Basic " + authToken
-        },
+        headers: headers
     });
 }
